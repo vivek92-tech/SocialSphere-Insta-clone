a1f0658e8bf049880c6caa3121f0296a19439c05
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-11-21 17:10:29 -0500

    configuration of postgreSQL

--- backend/socialSphere/socialSphere/settings.py
+++ backend/socialSphere/socialSphere/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'socialSphere.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'socialsphere',
+        'USER': 'postgres',
+        'PASSWORD': 'post321',
+        'HOST': 'localhost'
     }
 }
 
