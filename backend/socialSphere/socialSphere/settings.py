7e2a38242aff9d00920a806edd81dd15ce5f8e68
vivek92-tech <v.kumar75@vikes.csuohio.edu>
2020-11-21 15:44:17 -0500

    added custom user model

--- backend/socialSphere/socialSphere/settings.py
+++ backend/socialSphere/socialSphere/settings.py
@@ -38,11 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #3rd party app
+    # 3rd party app
     'rest_framework',
-
-    #local app
-    
+    # local app
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
